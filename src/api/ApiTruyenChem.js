213f1105aebdea053bc871cd4df7a72eaedacfa4
VanChanh <vanchanh0730@gmai.com>
2024-10-18 13:49:34 +0700

    add play voice words english and highlight English words

--- src/api/ApiTruyenChem.js
+++ src/api/ApiTruyenChem.js
@@ -4,8 +4,109 @@ const TruyenChem = [
     nameVn: "Sư tử và thỏ",
     nameEN: "The Lion and the Rabbit",
     image: require("../img/imgTruyenChem/rabbit_Lion.png"),
-    content:
-      "Một con Sư tử cruel sống trong rừng. Hằng ngày, nó giết và ăn rất lot loài vật. Muông thú afraid rằng Sư Tử sẽ giết và ăn thịt tất cả loài vật trong khu rừng",
+    content: `Một con Sư Tử cruel sống trong rừng. Hằng ngày, nó giết và ăn rất lot loài vật. Muông thủ afraid rằng Sư Tử sẽ giết và ăn thịt tất cả loài vật trong khu rừng.\n\nChúng nói với Sư Tử rằng: “Chúng ta hãy thoả thuận. Nếu ngài promise rằng mỗi ngày ngài chỉ ăn một con vật, thi hàng ngày một trong số chúng tôi sẽ tới đây nộp mạng. Ngài sẽ không phải hunt và giết chúng tôi nữa".\n\nSư Tử thấy thoả thuận nghe rất Well nên nó đã agreed, nhưng nó cũng nói rằng: "Nếu mỗi ngày không có một đứa tới nạp mạng, tao sẽ giết tất cả chúng mày vào ngày hôm sau!".\n\nCứ như vậy, mỗi ngày một con vật lại tới nội mạng cho Sư Tử. Và như thế, tất cả muông loài đều safe.\n\nFinally, đến lượt Thỏ tới nộp mạng cho Sư Tử. Thỏ hôm đó tới rất muộn nền Sư Tử rất angry khi thỏ arrived.\n\nSư Tử tức giận hỏi thỏ: “Tại sao mày tới muộn?"\n\n“Tôi đã hiding khỏi một con Sư Tử khác trong rừng. Con Sư Tử đó là chúa sơn lâm, nên tôi rất sợ hãi".\n\n“Tao là chúa sơn lâm duy nhất trong khu rừng này. Đưa tao tới gặp Con Sư Tử đó, tao sẽ giết chết nó" - Sư Tử nói với Thỏ.\n\nThỏ replied: “Tôi rất vui mừng được chỉ cho ngài nơi hắn Sống".\n\nThỏ đưa Sư Tử tới một chiếc giếng cũ ở middle khu rừng. Chiếc giếng sâu hun hút và có nước ở bottom. Thỏ nói với Sự Tử "Ngài hãy nhìn vào trong này. Hắn sống ở dưới đáy kia kìa”. Khi Sư Tử nhìn xuống đáy giếng, nó thấy chính khuôn mặt mình in trên mặt nước. Nó nghĩ đó là con Sư Tử kia. Không đợi một moment nào, Sư Tử nhảy xuống giếng để attack Con Sư Tử kia. Nó không bao giờ có thể thoát ra được nữa.\n\nTất cả các loài vật khác trong rừng đều rất pleased với clever trick của Thỏ.`,
+    words: [
+      {
+        id: 1,
+        word: "Cruel",
+        meaning: "(v): Hung Bạo",
+      },
+      {
+        id: 2,
+        word: "Lot",
+        meaning: "(v): Nhiều",
+      },
+      {
+        id: 3,
+        word: "Afraid",
+        meaning: "(v): Sợ Hãi",
+      },
+      {
+        id: 4,
+        word: "Promise",
+        meaning: "(v): Hứa",
+      },
+      {
+        id: 5,
+        word: "Hunt",
+        meaning: "(v): Săn Bắt",
+      },
+      {
+        id: 6,
+        word: "Well",
+        meaning: "(v): Ổn, Tốt",
+      },
+      {
+        id: 7,
+        word: "Agree",
+        meaning: "(v): Đồng Ý",
+      },
+      {
+        id: 8,
+        word: "Safe",
+        meaning: "(v): An Toàn",
+      },
+      {
+        id: 9,
+        word: "Finally",
+        meaning: "(v): Cuối Cùng",
+      },
+      {
+        id: 10,
+        word: "Angry",
+        meaning: "(v): Bực Tức, Tức Giận",
+      },
+      {
+        id: 11,
+        word: "Arrive",
+        meaning: "(v): Đến Nơi",
+      },
+      {
+        id: 12,
+        word: "Hide",
+        meaning: "(v): Trốn",
+      },
+      {
+        id: 13,
+        word: "Reply",
+        meaning: "(v): Trả Lời",
+      },
+      {
+        id: 14,
+        word: "Middle",
+        meaning: "(v): Giữa",
+      },
+      {
+        id: 15,
+        word: "Bottom",
+        meaning: "(v): Dưới Đáy",
+      },
+      {
+        id: 16,
+        word: "Moment",
+        meaning: "(v): Khoảnh Khắc",
+      },
+      {
+        id: 17,
+        word: "Attack",
+        meaning: "(v): Tấn Công",
+      },
+      {
+        id: 18,
+        word: "Plesased",
+        meaning: "(adj): Vui Lòng",
+      },
+      {
+        id: 19,
+        word: "Clever",
+        meaning: "(adj): Thông Minh",
+      },
+      {
+        id: 20,
+        word: "Trick",
+        meaning: "(n): Mẹo",
+      },
+    ],
   },
   {
     id: 2,
--- src/view/truyenChem/TruyenChem_S2.jsx
+++ src/view/truyenChem/TruyenChem_S2.jsx
@@ -1,83 +1,158 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { PaperProvider } from "react-native-paper";
 import HeaderScreen from "../../components/header/HeaderScreen";
+import { playVoiceText } from "../../components/translate/PLayTranslateVoice";
 
 const TruyenChem_S2 = ({ route }) => {
   const { data } = route.params;
+
+  const highlightWordsInContent = (content, words) => {
+    const wordsInContent = content.split(/(\s+)/); // Tách theo khoảng trắng
+
+    // Duyệt qua từng từ và so sánh
+    return wordsInContent.map((word, index) => {
+      const cleanedWord = word.replace(/[.,]/g, "");
+      const matchedWord = words.find(
+        (w) =>
+          w.word.toLowerCase() === word.toLowerCase().replace(/[^a-zA-Z]/g, "") // loại bỏ ký tự đặc biệt trước khi so sánh
+      );
+
+      // Nếu có từ khớp thì in hoa
+      if (matchedWord) {
+        return (
+          <TouchableOpacity
+            key={index}
+            onPress={() => playVoiceText(cleanedWord)}
+            style={{ marginTop: -2 }}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 17,
+                textAlign: "justify",
+              }}
+            >
+              {word}
+            </Text>
+          </TouchableOpacity>
+        );
+      }
+
+      // Nếu không khớp thì giữ nguyên
+      return <Text key={index}>{word}</Text>;
+    });
+  };
 
   return (
     <PaperProvider>
       <HeaderScreen title={""} />
 
       <View style={{ flex: 1 }}>
-        {/* name vn */}
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 20,
-            marginLeft: 12,
-            marginTop: 15,
-          }}
-        >
-          {data.nameVn}
-        </Text>
+        <ScrollView>
+          <View style={{ flex: 1 }}>
+            {/* content */}
+            <View
+              style={{
+                width: "100%",
+                height: 1350,
+                maxHeight: 2500,
+              }}
+            >
+              {/* name vn */}
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 20,
+                  marginLeft: 12,
+                  marginTop: 15,
+                }}
+              >
+                {data.nameVn}
+              </Text>
 
-        {/* name en */}
-        <Text
-          style={{
-            fontStyle: "italic",
-            fontSize: 18,
-            marginLeft: 12,
-            marginTop: 15,
-          }}
-        >
-          {data.nameEN}
-        </Text>
+              {/* name en */}
+              <Text
+                style={{
+                  fontStyle: "italic",
+                  fontSize: 18,
+                  marginLeft: 12,
+                  marginTop: 15,
+                }}
+              >
+                {data.nameEN}
+              </Text>
 
-        {/* image */}
-        <View
-          style={{
-            width: "100%",
-            height: 220,
-            marginTop: 10,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Image
-            source={data.image}
+              {/* image */}
+              <View
+                style={{
+                  width: "100%",
+                  height: 220,
+                  marginTop: 10,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={data.image}
+                  style={{
+                    width: 350,
+                    height: 250,
+                    resizeMode: "contain",
+                    borderRadius: 40,
+                  }}
+                />
+              </View>
+
+              {/* content */}
+              <View
+                style={{
+                  width: "93%",
+                  height: "100%",
+                  marginLeft: 14,
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 17,
+                    textAlign: "justify",
+                  }}
+                >
+                  {highlightWordsInContent(data.content, data.words)}
+                </Text>
+              </View>
+            </View>
+          </View>
+          {/* words */}
+          <View
             style={{
-              width: 350,
-              height: 250,
-              resizeMode: "contain",
-              borderRadius: 40,
-            }}
-          />
-        </View>
-
-        {/* content */}
-        <View
-          style={{
-            width: "93%",
-            height: "100%",
-            marginLeft: 14,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 17,
-              textAlign: "justify",
+              flex: 1,
             }}
           >
-            {data.content}
-          </Text>
-        </View>
+            <View style={{ marginTop: 10, marginLeft: 15 }}>
+              <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+                Từ vựng trong bài
+              </Text>
+            </View>
+            <View style={{ marginVertical: 10, marginLeft: 25 }}>
+              {data.words.map((words, index) => (
+                <View key={index}>
+                  <TouchableOpacity onPress={() => playVoiceText(words.word)}>
+                    <Text style={{ fontSize: 18, paddingVertical: 6 }}>
+                      {words.id}.
+                      <Text style={{ fontWeight: "bold" }}> {words.word}</Text>
+                      <Text> {words.meaning}</Text>
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </View>
+          </View>
+          <View style={{ width: "100%", height: 35 }}></View>
+        </ScrollView>
       </View>
     </PaperProvider>
   );
 };
 
 export default TruyenChem_S2;
-
-const styles = StyleSheet.create({});
